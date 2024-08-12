092ed857b73538b37f4de717015b59a28ac5f725
Johaan-Paul <johaanp2008@gmail.com>
2024-08-12 10:42:06 +0000

    Update the index.html and the 404.html pages to have a better nav bar + header + content + footer(Not Complete)

---
+++ global.js
@@ -0,0 +1,5 @@
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTopFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
