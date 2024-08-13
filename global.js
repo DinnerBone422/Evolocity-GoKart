6488ee46cd53626247276c35809c78799c1a9704
Johaan-Paul <johaanp2008@gmail.com>
2024-08-13 10:24:52 +0000

    Update the Navigation

--- global.js
+++ global.js
@@ -1,4 +1,18 @@
-// When the user clicks on the button, scroll to the top of the document
+var menuState = 0;
+// When this function is called the navigation menu is opened
+function openCloseMenuFunction() {
+    if (menuState == 0) {
+        document.getElementById("menu").style.transform = "translateX(0)";
+        document.getElementById("MenuOpenCloseButton").innerHTML = "close";
+        menuState = 1;
+    } else {
+        document.getElementById("menu").style.transform = "translateX(100%)";
+        document.getElementById("MenuOpenCloseButton").innerHTML = "menu";
+        menuState = 0;
+    }
+}
+
+// When this function is called the website will scroll back to the top
 function scrollToTopFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
