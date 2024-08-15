47514840bc4e388fa5a77ff97769da2f7b5dc9cc
Johaan-Paul <johaanp2008@gmail.com>
2024-08-15 06:07:10 +0000

    Fix the Issues with the Scroll to top button showing when page is at the top when the page is being refreshed or opened.

--- global.js
+++ global.js
@@ -1,29 +1,30 @@
 //Set Variables
-var menuState = 0;
+let menuState = 0; // When this var is 0 the menu is closed
+
+// When the page is loaded, run the scroll function
+window.onload = function() {scrollFunction()};
+
+// When the user scrolls, run the scroll function
+window.onscroll = function() {scrollFunction()};
+
 
 // When this function is called the navigation menu is opened
 function openCloseMenuFunction() {
-  if (menuState == 0) { //Menu Opened
-    document.getElementById("menu").style.display = "block";
+  if (menuState === 0) { //Menu Opened
     document.getElementById("menu").style.transform = "translateX(0)";
-    document.getElementById("MenuOpenCloseButton").innerHTML = "close";
-    
+    document.getElementById("MenuOpenCloseButton").innerHTML = "close"; 
+    document.getElementById("ScrollToTopButton").style.display = "none";
     menuState = 1;
   } else { //Menu Closed
-    document.getElementById("menu").style.display = "none";
     document.getElementById("menu").style.transform = "translateX(100%)";
     document.getElementById("MenuOpenCloseButton").innerHTML = "menu";
+    scrollFunction();
     menuState = 0;
   }
 }
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
-    console.log("body" + document.body.scrollTop);
-    console.log("documentElement" + document.documentElement.scrollTop);
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("ScrollToTopButton").style.display = "block";
   } else {
     document.getElementById("ScrollToTopButton").style.display = "none";
